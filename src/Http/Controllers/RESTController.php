3b66941cc458bfbbdadc74d72b9083fa139032f6
SkyaTura <antonio@skyatura.com.br>
2017-06-18 03:28:35 -0300

    chore(RESTController): Use FilterTrait

--- src/Http/Controllers/RESTController.php
+++ src/Http/Controllers/RESTController.php
@@ -4,10 +4,11 @@ namespace SkyaTura\EloquentREST\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use SkyaTura\EloquentREST\Traits\FilterTrait;
 use SkyaTura\EloquentREST\Traits\ResourceTrait;
 use SkyaTura\EloquentREST\Traits\ValidationTrait;
 
 class RESTController extends Controller
 {
-    use ResourceTrait, ValidationTrait;
+    use ResourceTrait, ValidationTrait, FilterTrait;
 }
